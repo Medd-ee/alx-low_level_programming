374fd469df24c3fe3d3fa544b997ff5a1a864eea
Medd-ee <odungideemediong@gmail.com>
2022-09-08 14:28:03 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,5 @@
-#include <stdio.h>                                                                                                                      
-
-                                                                                                                                        
-
+#include <stdio.h>                                                                                                               
+                                                                                                                      
 /**                                                                                                                                     
 * main - print the string in the put function                                                                                           
 *                                                                                                                                       
@@ -12,27 +10,15 @@
 int main(void)                                                                                                                          
 {                                                                                                                                       
         char c;                        
-
         int i;                                                                                                                          
-
-        long li;                                                                                                                        
-
+        long li;                                                                                                                       
         long long lli;                                                                                                                  
-
-        float f;                                                                                                                        
-
+        float f;                                                                                                                       
                                                                                                                                         
-
-        printf("Size of a char: %d byte(s)\n", sizeof(c));                                                                              
-
-        printf("Size of an int: %d byte(s)\n", sizeof(i));                                                                              
-
-        printf("Size of a long int: %d byte(s)\n", sizeof(li));                                                                         
-
-        printf("Size of a long long int: %d byte(s)\n", sizeof (lli));
-
-
-	printf("Size of a float %d byte(s)\n", sizeof (f));
+        printf("Size of a char: %ld byte(s)\n", sizeof(c));                                                                             
+        printf("Size of an int: %ld byte(s)\n", sizeof(i));                                                                                     printf("Size of a long int: %ld byte(s)\n", sizeof(li));                                                                        
+        printf("Size of a long long int: %ld byte(s)\n", sizeof (lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof (f));
 	return (0)
 }
 
