e83fa72f04dfc7e4f695a16ba6b50d20462eb470
Medd-ee <odungideemediong@gmail.com>
2022-09-15 20:53:25 -0700

    Print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,12 +14,12 @@ void print_number(int n)
 	x = n
 	if (n < 0)	
 	{
-		putchar(45);
+		_putchar(45);
 		x = -n;
 	}
 	if (x / 10)
 	{
 		print_number(x / 10);
 	}
-	putchar(x % 10 + '0');
+	_putchar(x % 10 + '0');
 }
