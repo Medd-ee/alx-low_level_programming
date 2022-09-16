ee61fd28b822afb969e9749c894e54c57c729db0
Medd-ee <odungideemediong@gmail.com>
2022-09-15 20:45:04 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,14 +1,13 @@
-#include <main.h>
 #include <stdio.h>
 
 /**
  *main - program that prints either number or fizz or buzz or fizzBuzz
  *
+ *
  *Return: returns 0
  */
 
 int main(void)
-
 {
 	int num = 1;
 
