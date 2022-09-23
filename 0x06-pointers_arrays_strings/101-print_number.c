5cc4f7028a552b8c03cbb1117fc9fa06b1a3d6cf
Medd-ee <odungideemediong@gmail.com>
2022-09-22 19:47:39 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,8 +14,10 @@ void print_number(int n)
 	                _putchar('-');                                                                                                        
 	                num = -num;                                                                                                           
 	        }                                                                                                                             
-	if ((num / 10) > 0)                                                                                                           
-                print_number(num / 10);                                                                                               
+	if ((num / 10) > 0)  
+		{		
+                	print_number(num / 10); 
+		}			
                                                                                                                         
         _putchar((num % 10) + '0');                                                                                                   
 }
