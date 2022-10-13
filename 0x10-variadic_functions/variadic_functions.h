b6e439a909b3a82542562250f0eddd8b59fbf2ca
Medd-ee <odungideemediong@gmail.com>
2022-10-13 12:38:17 -0700

    sum them all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,7 +8,7 @@
  * @f: print function
  */
 
-typedef struct print 
+typedef struct print
 {
 	char *t;
 	void (*f)(va_list);
@@ -18,6 +18,6 @@ int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char* const format, ...);
+void print_all(const char * const format, ...);
 
-#endif /*_VARIADIC_FUNCTIONS_H_*/
+#endif /* _VARIADIC_FUNCTIONS_H_ */
